1dcbcd68fef9f2108238c00ce335538b00180790
Giovannie <giovanni.pontrelli@outlook.it>
2025-11-21 12:28:13 +0100

    primi progetti push

---
+++ 11-18-25/Program.cs
@@ -0,0 +1,255 @@
+﻿/*.net 6:
+ See https://aka.ms/new-console-template for more information
+using System;
+.net 5 darebbe:
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();   // <-- tiene aperta la console
+        }
+    }
+}
+______________________________________________________________________________________________
+ .csproj è il file xml di configurazione dle progetto C# in Visual Studio/*
+/* 
+ Scrivere un programma che, richiesti in input 3 numeri interi, visualizzi a seconda dei casi una delle seguenti risposte:
+Tutti uguali
+Due uguali e uno diverso (indicando i numeri uguali)
+Tutti diversi
+ */
+using System;
+namespace MyApp
+{
+    /* internal class Program
+     {
+         static void Main(string[] args)
+         {
+             Console.WriteLine("Hello World!");
+             int a, b, c;
+             Console.Write("a=");
+             a = int.Parse(Console.ReadLine());
+             Console.Write("b=");
+             b = int.Parse(Console.ReadLine());
+             Console.Write("c=");
+             c = int.Parse(Console.ReadLine());
+
+             if (a == b && b == c)
+             {
+                 Console.WriteLine("Tutti uguali");
+             }
+             else if (a == b || a == c || b == c)
+             {
+                 if (a == b)
+                 {
+                     Console.WriteLine($"Due uguali: {a} e uno diverso: {c}");
+                 }
+                 else if (a == c)
+                 {
+                     Console.WriteLine($"Due uguali: {a} e uno diverso: {b}");
+                 }
+                 else // b==c
+                 {
+                     Console.WriteLine($"Due uguali: {b} e uno diverso: {a}");
+                 }
+             }
+             else
+             {
+                 Console.WriteLine("Tutti diversi");
+             }
+             Console.Write("Press any key for continue ...");
+             Console.ReadKey();
+
+
+         }
+     }
+    */
+
+
+    /*
+     *Scrivere un pro0gramma che implementi una semplice calcolatrice. Il programma deve chiedere all'utente di inserire due numeri e l'operazione da eseguire (+,-,*,/)
+     */
+    /*
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+          
+            double num1, num2, result = 0;
+            char operazione;
+            Console.Write("Inserisci il primo numero: ");
+            num1 = double.Parse(Console.ReadLine());
+            if(num1 == null )
+            {
+                Console.WriteLine("Errore: inserire un numero valido.");
+                Console.Write("Press any key for continue ...");
+                Console.ReadKey();
+                return; // esce dal programma
+            }
+            Console.Write("Inserisci il secondo numero: ");
+            num2 = double.Parse(Console.ReadLine());
+            if (num2 == null)
+            {
+                Console.WriteLine("Errore: inserire un numero valido.");
+                Console.Write("Press any key for continue ...");
+                Console.ReadKey();
+                return; // esce dal programma
+            }
+            Console.Write("Inserisci l'operazione desiderata tra (+,-,*,/) ");
+            operazione = char.Parse(Console.ReadLine());
+
+            // con if:
+
+            /* if(operazione == '+')
+             {
+                 result = num1 + num2;
+                 Console.Write("Il risultato è" + result);
+             }
+             else if(operazione == '-')
+             {
+                 result = num1 - num2;
+                 Console.Write("Il risultato è" + result);
+             }
+             else if(operazione == '*')
+             {
+                 result = num1 * num2;
+                 Console.Write("Il risultato è" + result);
+             }
+             else if(operazione == '/')
+             {
+                 if(num2 != 0)
+                 {
+                     result = num1 / num2;
+                     Console.Write("Il risultato è" + result);
+                 }
+                 else
+                 {
+                     Console.WriteLine("Errore: divisione per zero non consentita.");
+                     Console.Write("Press any key for continue ...");
+                     Console.ReadKey();
+                     return; // esce dal programma
+                 }
+             }
+             else
+             {
+                 Console.WriteLine("Operazione non valida.");
+                 Console.Write("Press any key for continue ...");
+                 Console.ReadKey();
+                 return; // esce dal programma
+             }
+            */
+
+    // con switch:
+    /*
+    switch (operazione)
+    {
+        case '+':
+            result = num1 + num2;
+           // Console.WriteLine("Il risultato è " + result);
+            break;
+        case '-':
+            result = num1 - num2;
+           // Console.WriteLine("Il risultato è " + result);
+            break;
+        case '*':
+            result = num1 * num2;
+            //Console.WriteLine("Il risultato è " + result);
+            break;
+        case '/':
+            result = num1 / num2;
+            if (num2 != 0)
+            {
+                result = num1 / num2;
+                Console.Write("Il risultato è" + result);
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Errore: divisione per zero non consentita.");
+                Console.Write("Press any key for continue ...");
+                Console.ReadKey();
+                break;
+            }
+        default:
+            //si mette il dollaro perchè senza non riuscirebbe a trasmettere il valore alla graffa (segnaposto). l'alternativa sarebbe la concatenazione di stringhe                    Console.WriteLine($"Operazione: {operazione} non corretta! è possibile inserire solo uno tra questi:(+,-,*,/) ");
+            break;
+
+    }
+    */
+    /*
+     variante con unico log (si commentano i log nello switch):
+    */
+    /*
+    Console.WriteLine("Il risultato è " + result);
+
+
+    Console.Write("Press any key for continue ...");
+    Console.ReadKey();
+
+
+}
+} */
+
+
+    /*
+    inserire n interi da tastiera da sommare
+    */
+    /*
+    dare la somma di tutti i numeri pari compresi tra 5 e 30
+    */
+    /*
+     alla fine del ciclo chiedere all'utente se vuyole continuare a ciclare e continuare a sommare nuovi numeri
+     */
+    internal class Program
+         {
+             static void Main(string[] args)
+             {
+            int cont = 0;
+            while (cont == 0)
+            {
+                Console.WriteLine("Inserisci il numero di volte che vuoi sommare numeri");
+                 int a;
+                 Console.Write("numero cicli =");
+                 a = int.Parse(Console.ReadLine());
+                 int res = 0;
+                 int s = 1;
+                 int res2 = 0;
+            while (a > 0)
+                {
+
+                    Console.WriteLine($"Inserisci il {s}° numero intero da sommare");
+                    int num;
+                    Console.Write("numero =");
+                    num = int.Parse(Console.ReadLine());
+              if(num % 2 == 0 && num >= 5 && num <= 30)
+                {
+                    res2 += num;
+                }
+                    a--;
+                    s++;
+                    res += num;
+                }
+                Console.WriteLine($"La somma dei numeri inseriti è: {res}" );
+                Console.WriteLine($"La somma dei numeri inseriti pari e compresi trra 5 e 30 è: {res2}");
+
+                string scelta;
+                Console.Write("Vuoi testare altri numeri? ( premi 1 per confermare, altrimenti un altro tasto.)");
+                scelta = Console.ReadLine();
+                if (scelta == "1") { 
+                    cont = 0; 
+                }
+                else
+                {
+                    cont = 1;
+                }
+               // Console.ReadKey(); fa dare due volte invio o tasto
+
+
+            }
+         }
+    }
+}
---
+++ 11-20-25/Program.cs
@@ -0,0 +1,235 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            programma nel quale si chiedono due numeri e questi due numeri si sommano mostrandola
+            */
+            /* Console.WriteLine("Inserisci due numeri da sommare.");
+             Console.WriteLine("Inserisci il primo numero:");
+             int x = int.Parse(Console.ReadLine());
+             bool successo = false;
+             Console.WriteLine("Inserisci il secondo numero:");
+             int y = int.Parse(Console.ReadLine());
+             int z = x + y;
+             if ( z != null && z != 0)
+             {
+                 successo = true;
+
+             }
+             else
+             {
+                 successo = false;
+                 Console.WriteLine("C'è stato un errore nel calcolo della somma. verifica i numeri in input");
+             }
+             Console.WriteLine("La somma dei due numeri è: " + z);
+             Console.ReadLine(); */
+
+            /*fatto anche con while e do while e if per verificare il fatto che i numeri inseriti siano int :*/
+            /* PS:  sono pigro non l'ho fatto */
+            /* versione con switch + implementazioni: */
+            int scelta;
+            double x, y,ris;
+            do
+            {
+                Console.WriteLine("menù");
+                Console.WriteLine("1. somma");
+                Console.WriteLine("2. divisione");
+                Console.WriteLine("3. sottrazione");
+                Console.WriteLine("4. moltiplicazione");
+                Console.WriteLine("5. chiedi media");
+                Console.WriteLine("6. inserimento dati in array");
+                Console.WriteLine("7. continue su dowhile");
+                Console.WriteLine("8. operatore ternario");
+                Console.WriteLine("0. esci");
+                scelta = int.Parse(Console.ReadLine());
+                switch (scelta)
+                {
+                    case 1:
+                        Console.WriteLine("Inserisci il primo numero:");
+                        x = CheckNumber(Console.ReadLine());
+                        Console.WriteLine("Inserisci il secondo numero:");
+                        y = CheckNumber(Console.ReadLine());
+                        // int ris = x + y;
+                        ris = somma(x, y);
+                        Console.WriteLine($"il risultato è {ris}. ");
+                        break;
+                    case 2:
+
+                        Console.WriteLine("Inserisci il primo numero:");
+                        x = CheckNumber(Console.ReadLine());
+                        Console.WriteLine("Inserisci il secondo numero:");
+                        y = CheckNumber(Console.ReadLine());
+                        // double ris2 = z / a;
+                         ris = rapporto(x,y);
+                        Console.WriteLine($"il risultato è {ris}. ");
+                        break;
+                    case 3:
+
+                        Console.WriteLine("Inserisci il primo numero:");
+                        x = CheckNumber(Console.ReadLine());
+                        Console.WriteLine("Inserisci il secondo numero:");
+                        y = CheckNumber(Console.ReadLine());
+                        //double ris3 = b - c;
+                        ris = sottrazione(x,y);
+                        Console.WriteLine($"il risultato è {ris}. ");
+                        break;
+                    case 4:
+
+                        Console.WriteLine("Inserisci il primo numero:");
+                        x = CheckNumber(Console.ReadLine());
+                        Console.WriteLine("Inserisci il secondo numero:");
+                        y = CheckNumber(Console.ReadLine());
+                        // double ris4 = d * e;
+                        ris = moltiplicazione(x,y);
+                        Console.WriteLine($"il risultato è {ris}. ");
+                        break;
+                    case 5:
+                        chiediMedia();
+                        break;
+                    case 6:
+
+                        /*
+                    Arrray
+                    */
+
+                        int[] numeri = new int[5];
+                        Console.WriteLine($"{numeri[2]}");
+
+                        int[] numeriPari = { 2, 4, 6, 8, 10 };
+                        int i = 0;
+                        for (i = 0; i < numeriPari.Length; i++)
+                        {
+                            Console.WriteLine($"il numero in posizione {i} è {numeriPari[i]}");
+                        }
+
+                        //programma: chiedi entro 20 numeri quanti numeri vuole inserire (n di prima amssimo 20)
+                        //metti i valori in un array
+                        //fai la media
+                        Console.WriteLine("Quanti numeri vuoi inserire? (Massimo 20)");
+                        double n = CheckNumber(Console.ReadLine());
+                        if (n > 20 && n > 0)
+                        {
+                            Console.WriteLine("troppi numeri da inserire o valore negativo, il massimo è 20.");
+                            return;
+                        }
+                        double[] arr = new double[20];
+                        int j = 0;
+                        while (j < n)
+                        {
+                            Console.WriteLine($"inserisci il {j + 1}° numero.");
+                            double nx = CheckNumber(Console.ReadLine());
+
+                            arr[j] = nx;
+                            j++;
+                        }
+                        Console.WriteLine("Ecco l'array:");
+                        for (int s = 0; s < arr.Length; s++)
+                        {
+                            Console.WriteLine($"{arr[s]}");
+                        }
+                        Console.WriteLine($"Ecco la media: {mediaArr(arr)} ");
+                        break;
+                    case 7:
+                        /*
+                        dowhile che chiede di inserire un numero positivo all'utente ed esca quando è negativo
+                        */
+                        Boolean a = true;
+                        while (a)
+                        {
+                            Console.WriteLine("inserisci un numero");
+                            int s = int.Parse(Console.ReadLine());
+
+
+                            if (s < 0)
+                            {
+                                Console.WriteLine($"hai inserito {s} non va bene");
+                                return;
+                            }
+                            
+                        }
+                        break;
+                    case 8:
+                        Console.WriteLine("inserisci un numero");
+                        double numero = CheckNumber(Console.ReadLine());
+                        string isPari = numero % 2 == 0 ? "il numero è pari" : "il numero non è pari";
+                        Console.WriteLine(isPari);
+                        break;
+                    case 0:
+                        Console.WriteLine("In chiusura...");
+                            return;
+                    default:
+                        Console.WriteLine("Scelta non valida");
+                        break;
+                } 
+            } while (scelta != 0);
+
+        }
+        static double somma(double a, double b)
+        {
+            return a + b;
+        }
+        static double sottrazione(double a, double b)
+        {
+            return a - b;   
+        }
+        static double rapporto(double a, double b)
+        {
+            return a / b;
+        }
+        static double moltiplicazione(double a, double b)
+        {
+            return a * b;
+        }
+
+        static double CheckNumber(string input)
+        {
+            double numero;
+
+            while ( !double.TryParse(input, out numero)){
+                Console.WriteLine("input nojn valido");
+                input = Console.ReadLine();
+            }
+            return numero;
+        }
+
+        /*
+         funzione media e richiesta numeri in input
+         */
+
+        static double media(double n, double somma)
+        {
+            return somma / n;
+        }
+        static void chiediMedia()
+        {
+            double n, somma = 0, i = 1;
+            Console.WriteLine("di quanti numeri vuoi fare la media?");
+            n = CheckNumber(Console.ReadLine());
+            for (int j = 0; j <= n; j++)
+            {
+                Console.WriteLine($"inserisci il {i}° numero");
+                double a = CheckNumber(Console.ReadLine());
+                somma += a;
+                i++;
+            }
+            Console.WriteLine("la somma è " + somma);
+            Console.WriteLine("la media è " + media(n,somma));
+        }
+
+        static double mediaArr(double []array)
+        {
+            double somma = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                somma += array[i];
+            }
+            return media(array.Length,somma);
+        }
+       
+    }
+}
