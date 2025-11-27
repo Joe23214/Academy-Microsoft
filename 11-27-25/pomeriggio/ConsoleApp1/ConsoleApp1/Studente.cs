571ec410fb8f27cada35b2db955e93c6719e76e8
Giovannie <giovanni.pontrelli@outlook.it>
2025-11-27 15:00:30 +0100

    aggiunta refactor

---
+++ 11-27-25/pomeriggio/ConsoleApp1/ConsoleApp1/Studente.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Studente
+    {
+        private string nome;
+        private string cognome;
+        private string matricola;
+
+        private List<int> voti;
+
+        public Studente(string nome, string cognome, string matricola)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.matricola = matricola;
+            voti = new List<int>();
+        }
+
+        public string Nome { get => nome; }
+        public string Cognome { get => cognome; }
+        public string Matricola { get => matricola; }
+
+        public double Media
+        {
+            get
+            {
+                if (voti.Count == 0) return 0;
+                double somma = 0;
+                foreach (int v in voti)
+                    somma += v;
+                return somma / voti.Count;
+            }
+        }
+
+        public int NumeroVoti => voti.Count;
+
+        public void AggiungiVoto(int voto)
+        {
+            voti.Add(voto);
+        }
+
+        public void RimuoviUltimoVoto()
+        {
+            if (voti.Count > 0)
+                voti.RemoveAt(voti.Count - 1);
+        }
+
+        public void StampaLibretto()
+        {
+            Console.WriteLine($"Libretto {Nome} {Cognome} (Matricola: {Matricola})");
+            foreach (int voto in voti)
+                Console.WriteLine($"- {voto}");
+            Console.WriteLine($"Media: {Media:F2}");
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} {Cognome} (Mat: {Matricola}) - Media: {Media:F2}, Voti: {NumeroVoti}";
+        }
+    }
+}
---
+++ 11-27-25/pomeriggio/ConsoleApp1/ConsoleApp1/StudentiController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class StudentiController
+    {
+        public Dictionary<string, Studente> studenti;
+
+        public StudentiController()
+        {
+            studenti = new Dictionary<string, Studente>();
+        }
+
+        public bool AggiungiStudente(string nome, string cognome, string matricola)
+        {
+            if (studenti.ContainsKey(matricola))
+                return false;
+
+            studenti.Add(matricola, new Studente(nome, cognome, matricola));
+            return true;
+        }
+
+        public Studente Cerca(string matricola)
+        {
+            studenti.TryGetValue(matricola, out Studente trovato);
+            return trovato;
+        }
+
+        public bool AggiungiVoto(string matricola, int voto)
+        {
+            if (studenti.TryGetValue(matricola, out Studente s))
+            {
+                s.AggiungiVoto(voto);
+                return true;
+            }
+            return false;
+        }
+
+        public IEnumerable<Studente> GetAll()
+        {
+            foreach (var item in studenti.Values)
+                yield return item;
+        }
+
+        public Studente MediaPiuAlta()
+        {
+            Studente best = null;
+
+            foreach (var s in studenti.Values)
+            {
+                if (best == null || s.Media > best.Media)
+                    best = s;
+            }
+            return best;
+        }
+    }
+}
