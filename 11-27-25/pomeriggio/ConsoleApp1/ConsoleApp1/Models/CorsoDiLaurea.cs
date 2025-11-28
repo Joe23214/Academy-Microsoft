0366c696b3d1e7ad92fc7523a669f032fbaa5448
Giovannie <giovanni.pontrelli@outlook.it>
2025-11-28 14:29:28 +0100

    sistemato in pattern MVC + repository che gestisce dati e interfaccia

---
+++ 11-27-25/pomeriggio/ConsoleApp1/ConsoleApp1/Models/CorsoDiLaurea.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class CorsoDiLaurea
+    {
+        public string Codice { get; }
+        public string Nome { get; }
+        public List<Professore> Professori { get; }
+        public List<string> Materie { get; }
+
+        public CorsoDiLaurea(string codice, string nome)
+        {
+            Codice = codice;
+            Nome = nome;
+            Professori = new List<Professore>();
+            Materie = new List<string>();
+        }
+
+        public override string ToString()
+        {
+            string profs = string.Join(", ", Professori.Select(p => p.Nome + " " + p.Cognome + $" ({p.Materia})"));
+            return $"{Nome} (Cod: {Codice}) - Professori: {profs}";
+        }
+    }
+
+}
---
+++ 11-27-25/pomeriggio/ConsoleApp1/ConsoleApp1/Models/Studente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class Studente
+    {
+        public string Nome { get; }
+        public string Cognome { get; }
+        public string Matricola { get; }
+        public CorsoDiLaurea CorsoIscritto { get; set; }
+
+        public Studente(string nome, string cognome, string matricola)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Matricola = matricola;
+        }
+
+        public override string ToString()
+        {
+            string corso = CorsoIscritto != null ? CorsoIscritto.Nome : "Non iscritto";
+            return $"{Nome} {Cognome} (Mat: {Matricola}) - Corso: {corso}";
+        }
+    }
+
+}
